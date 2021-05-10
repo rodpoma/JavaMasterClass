0f05f1dcf1b989d248e4642cad4ac626f7add4f3
rodpoma <rodpoma@gmail.com>
2021-05-10 13:16:00 -0400

    integers and operators

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //How to print
+        System.out.println("Hello, Rod!");
+
+        //int = integer
+        //Standard variables and Operators;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
